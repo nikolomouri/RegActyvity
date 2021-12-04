70625259d224882e30cf2f277989d8ff0680595f
nikolomouri <niko.lomouri.1@btu.edu.ge>
2021-12-04 13:40:36 +0400

    reg

--- app/src/main/java/com/example/registeractivity/RegisterActivity.kt
+++ app/src/main/java/com/example/registeractivity/RegisterActivity.kt
@@ -1,11 +1,10 @@
 package com.example.registeractivity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.ContactsContract
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
